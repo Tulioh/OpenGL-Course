07b87ea54cb8b3fcc01238c15d22bf40bc4f5cb3
Túlio Henrique <tuliohdvp@gmail.com>
2019-01-28 11:51:34 -0200

    Initial Commit

---
+++ opengl-course/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  opengl-course
+//
+//  Created by Túlio Henrique on 28/01/19.
+//  Copyright © 2019 Túlio Henrique. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
