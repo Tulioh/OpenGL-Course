df47d1771a72c6d6a7cb88674c28d3c77c7bf274
Túlio Henrique <tuliohdvp@gmail.com>
2019-02-03 15:41:23 -0200

    finished Texture class, MVP and some gui

--- opengl-course/Application.cpp
+++ opengl-course/Application.cpp
@@ -1,6 +1,8 @@
 #define GLEW_STATIC
 #include <GL/glew.h>
+
 #include <GLFW/glfw3.h>
+
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -12,6 +14,14 @@
 #include "IndexBuffer.h"
 #include "VertexArray.h"
 #include "Shader.h"
+#include "Texture.h"
+
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
 
 int main(void) {
     // Initialize GLFW
@@ -26,7 +36,7 @@ int main(void) {
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     
     // Create the Window
-    GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(940, 560, "Hello World", NULL, NULL);
     if (!window) {
         glfwTerminate();
         return -1;
@@ -34,7 +44,6 @@ int main(void) {
     
     // Attach window to OpenGL context
     glfwMakeContextCurrent(window);
-    
     glfwSwapInterval(1);
     
     if (glewInit() != GLEW_OK) {
@@ -43,12 +52,17 @@ int main(void) {
     
     std::cout << "GL Version " << glGetString(GL_VERSION) << std::endl;
     
+    ImGui::CreateContext();
+    ImGui::StyleColorsDark();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 150");
+    
     // Vertex to create an rectangle
     float positions[] = {
-        -0.5f, -0.5f, // 0
-         0.5f, -0.5f, // 1
-         0.5f,  0.5f, // 2
-        -0.5f,  0.5f, // 3
+        -50.0f, -50.0f, 0.0f, 0.0f, // 0
+         50.0f, -50.0f, 1.0f, 0.0f, // 1
+         50.0f,  50.0f, 1.0f, 1.0f, // 2
+        -50.0f,  50.0f, 0.0f, 1.0f // 3
     };
     
     unsigned int indices[] = {
@@ -56,25 +70,33 @@ int main(void) {
         2, 3, 0
     };
     
+    GLCall(glEnable(GL_BLEND));
+    GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+    
     // Create and bind vao
     VertexArray va;
     // Create the vertex buffer data and bind it to draw on screen
-    VertexBuffer vb(positions, 4 * 2 * sizeof(float));
+    VertexBuffer vb(positions, 4 * 4 * sizeof(float));
     
     // Links the buffer with the vao
     VertexBufferLayout layout;
+    layout.Push<float>(2);
     layout.Push<float>(2);
     va.AddBuffer(vb, layout);
     
     // Tell OpenGL how each Vertex are linked to create the rectangle
     IndexBuffer ib(indices, sizeof(indices));
     
+    glm::mat4 projection = glm::ortho<float>(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
+    glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
+    
     // Load shader source and load into program
     Shader shader("resources/shaders/Basic.shader");
     shader.Bind();
-
-    // get u_Color from shader to change dynamically
-    shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
+    
+    Texture texture("resources/textures/google-logo.png");
+    texture.Bind(0);
+    shader.SetUniform1i("u_Texture", 0);
     
     // Reset everything
     va.Unbind();
@@ -84,17 +106,37 @@ int main(void) {
     
     Renderer renderer;
     
+    glm::vec3 translationA(200, 200, 0);
+    glm::vec3 translationB(400, 200, 0);
+    
     float r = 0.0f;
     float increment = 0.05f;
     
     while(!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+        
         renderer.Clear();
         
-        // Use shader program to change the u_Color
-        shader.Bind();
-        shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
+        // Start the Dear ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
         
-        renderer.Draw(va, ib, shader);
+        {
+            glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
+            glm::mat4 mvp = projection * view * model;
+            shader.Bind();
+            shader.SetUniformMat4f("u_MVP", mvp);
+            renderer.Draw(va, ib, shader);
+        }
+        
+        {
+            glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
+            glm::mat4 mvp = projection * view * model;
+            shader.Bind();
+            shader.SetUniformMat4f("u_MVP", mvp);
+            renderer.Draw(va, ib, shader);
+        }
         
         // Logic to change color each frame
         if (r > 1.0f) {
@@ -104,11 +146,23 @@ int main(void) {
         }
         r += increment;
         
+        {
+            ImGui::Begin("Transform");
+            ImGui::SliderFloat3("Translation A", &translation.x, 0.0f, 960.0f);
+            ImGui::SliderFloat3("Translation B", &translation.x, 0.0f, 960.0f);
+            ImGui::End();
+        }
+        
+        // ImGui Render
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        
         // GLFW specific things to clear buffers and get input events
         glfwSwapBuffers(window);
-        glfwPollEvents();
     }
     
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
     glfwTerminate();
     return 0;
 }
--- opengl-course/Shader.h
+++ opengl-course/Shader.h
@@ -12,6 +12,8 @@
 #include <string>
 #include <unordered_map>
 
+#include "glm/glm.hpp"
+
 struct ShaderProgramSources {
     std::string VertexSources;
     std::string FragmentSources;
@@ -21,7 +23,7 @@ class Shader {
 private:
     std::string m_FilePath;
     unsigned int m_RendererID;
-    std::unordered_map<std::string, unsigned int> m_UniformLocationCache;
+    std::unordered_map<std::string, int> m_UniformLocationCache;
 public:
     Shader(const std::string& filename);
     ~Shader();
@@ -30,14 +32,15 @@ public:
     void Unbind() const;
     
     // Set uniforms
+    void SetUniform1i(const std::string& name, int value);
     void SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3);
-    
+    void SetUniformMat4f(const std::string& name, const glm::mat4& matrix);
 private:
     ShaderProgramSources ParseShader(const std::string& filePath);
     unsigned int CompileShader(unsigned int type, const std::string& source);
     unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader);
     
-    unsigned int GetUniformLocation(const std::string& name);
+    int GetUniformLocation(const std::string& name);
 };
 
 #endif /* Shader_h */
